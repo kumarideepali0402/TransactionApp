4b032339905abbfcdfa35d0b13d309e40276e986
Deepali <deepalikumari62004@gmail.com>
2025-11-21 20:01:38 +0530

    removed .env file

--- Backend/server.js
+++ Backend/server.js
@@ -3,5 +3,6 @@ const app = require("./src/app")
 const connectDB = require("./src/db/db")
 
 connectDB();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>console.log("server is running in port 3000"));
+app.listen(port, ()=>console.log("server is running in port 3000"));
